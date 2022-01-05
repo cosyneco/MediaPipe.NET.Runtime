5be54fbf550bc8371b533cc89183eb155077afca
Speykious <speykious@gmail.com>
2022-01-05 04:39:34 +0100

    Remove analyzers

--- build.py
+++ build.py
@@ -17,7 +17,6 @@ _BAZEL_BIN_PATH = 'bazel-bin'
 _BAZEL_OUT_PATH = 'bazel-out'
 _BUILD_PATH = 'build'
 _NUGET_PATH = '.nuget'
-_ANALYZER_PATH = os.path.join('Assets', 'Analyzers')
 _INSTALL_PATH = os.curdir
 
 _PROTOBUF_PATH = 'Mediapipe.Net.Framework.Protobuf'
@@ -154,12 +153,6 @@ class BuildCommand(Command):
     self.console.info('Installing built resources...')
     # _copytree fails on Windows, so run `cp -r` instead.
     self._copytree(_BUILD_PATH, _INSTALL_PATH)
-
-    # install analyzers
-    if self.analyzers:
-      self.console.info('Installing Roslyn Analyzers...')
-      for f in glob.glob(os.path.join(_NUGET_PATH, '**', 'analyzers', 'dotnet', 'cs', '*.dll'), recursive=True):
-        self._copy(f, _ANALYZER_PATH)
 
     self.console.info('Installed')
 
@@ -312,7 +305,6 @@ class UninstallCommand(Command):
     self.android = command_args.args.android
     self.ios = command_args.args.ios
     self.protobuf = command_args.args.protobuf
-    self.analyzers = command_args.args.analyzers
 
   def run(self):
     self._rmtree(_BUILD_PATH)
@@ -349,12 +341,6 @@ class UninstallCommand(Command):
         if not f.endswith('.meta'):
           self._remove(f)
 
-    if self.analyzers:
-      self.console.info('Uninstalling analyzers...')
-
-      for f in glob.glob(os.path.join(_ANALYZER_PATH, '*.dll'), recursive=True):
-        self._remove(f)
-
 
 class HelpCommand(Command):
   def __init__(self, args):
@@ -376,7 +362,6 @@ class Argument:
     build_command_parser.add_argument('--desktop', choices=['cpu', 'gpu'])
     build_command_parser.add_argument('--android', choices=['armv7', 'arm64', 'fat'])
     build_command_parser.add_argument('--ios', choices=['arm64'])
-    build_command_parser.add_argument('--analyzers', action=argparse.BooleanOptionalAction, default=False, help='Install Roslyn Analyzers')
     build_command_parser.add_argument('--compilation_mode', '-c', choices=['fastbuild', 'opt', 'dbg'], default='opt')
     build_command_parser.add_argument('--opencv', choices=['local', 'cmake'], default='local', help='Decide to which OpenCV to link for Desktop native libraries')
     build_command_parser.add_argument('--include_opencv_libs', action='store_true', help='Include OpenCV\'s native libraries for Desktop')
@@ -391,7 +376,6 @@ class Argument:
     uninstall_command_parser.add_argument('--android', action=argparse.BooleanOptionalAction, default=True)
     uninstall_command_parser.add_argument('--ios', action=argparse.BooleanOptionalAction, default=True)
     uninstall_command_parser.add_argument('--protobuf', action=argparse.BooleanOptionalAction, default=True)
-    uninstall_command_parser.add_argument('--analyzers', action=argparse.BooleanOptionalAction, default=True)
     uninstall_command_parser.add_argument('--verbose', '-v', action='count', default=0)
 
     self.args = self.argument_parser.parse_args()
