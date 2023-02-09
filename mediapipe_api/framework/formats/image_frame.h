7950be5060deddabe334f32bcc0b2040ea90181b
Ayane <ayane@vignetteapp.org>
2023-02-09 15:30:01 +0800

    Synchronize changes from MediaPipeUnityPlugin
    Signed-off-by: Ayane <ayane@vignetteapp.org>

--- mediapipe_api/framework/formats/image_frame.h
+++ mediapipe_api/framework/formats/image_frame.h
@@ -24,7 +24,8 @@ typedef void(Deleter)(uint8*);
 MP_CAPI(MpReturnCode) mp_ImageFrame__(mediapipe::ImageFrame** image_frame_out);
 MP_CAPI(MpReturnCode) mp_ImageFrame__ui_i_i_ui(mediapipe::ImageFormat::Format format, int width, int height, uint32 alignment_boundary,
                                                mediapipe::ImageFrame** image_frame_out);
-MP_CAPI(MpReturnCode) mp_ImageFrame__ui_i_i_i_Pui8(mediapipe::ImageFormat::Format format, int width, int height, int width_step, uint8* pixel_data, mediapipe::ImageFrame** image_frame_out);
+MP_CAPI(MpReturnCode) mp_ImageFrame__ui_i_i_i_Pui8_PF(mediapipe::ImageFormat::Format format, int width, int height, int width_step, uint8* pixel_data,
+                                                      Deleter* deleter, mediapipe::ImageFrame** image_frame_out);
 MP_CAPI(void) mp_ImageFrame__delete(mediapipe::ImageFrame* image_frame);
 MP_CAPI(bool) mp_ImageFrame__IsEmpty(mediapipe::ImageFrame* image_frame);
 MP_CAPI(MpReturnCode) mp_ImageFrame__SetToZero(mediapipe::ImageFrame* image_frame);
